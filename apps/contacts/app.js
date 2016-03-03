c6dff187096d90bbb186f4fa25407134aca854ed
Han Ni <nihan1994@163.com>
2016-03-03 12:37:29 +0000

    Finish Lab5

--- apps/contacts/app.js
+++ apps/contacts/app.js
@@ -1,1 +1,126 @@
-// TODO
+var ReactDOM = require('react-dom')
+    var React = require('react')
+
+    var ContactForm = React.createClass({
+        render: function(){
+          return (
+            <tr>
+              <td>
+              <input type="text" className="form-control" />
+              </td>
+              <td>
+              <input type="text" className="form-control"/>
+              </td>
+              <td>
+              <input type="text" className="form-control" />
+              </td>
+              <td>
+              <input type="button" className="btn btn-primary" value="Add"/>
+              </td>
+            </tr>
+            )
+        }
+      });
+
+    var Contact = React.createClass({
+          render: function(){
+            
+            var fields = [
+                     <td key={'name'} >{this.props.contacts.name}</td>,
+                      <td key={'address'}>{this.props.contacts.address}</td>,
+                      <td key={'phone_number'}>{this.props.contacts.phone_number}</td>
+                   ] ;
+              return (
+                      <tr >
+                         {fields}
+                         <td>
+                          <input type="button" className="btn btn-primary" 
+                                 value="Edit" />
+                      </td>
+                      <td>
+                         <input type="button" className="btn btn-primary" 
+                               value="Delete" />
+                      </td>
+                         
+                      </tr>
+
+                ) ;
+            }
+          });
+
+    var ContactList = React.createClass({
+          render: function(){
+               var contactRows = this.props.contacts.map(function(contact){
+                    return (
+                     <Contact key={contact.phone_number}  contacts={contact} />
+                      ) ;
+                  }.bind(this) );
+              return (
+                  <tbody >
+                      {contactRows}
+                      <ContactForm  />
+                  </tbody>
+                ) ;
+            }
+          });
+
+    var ContactsTable = React.createClass({
+          render: function(){
+              return (
+                <table className="table table-bordered">
+                      <thead>
+                        <tr>
+                        <th>Name</th>
+                        <th>Address</th>
+                        <th>Phone Number</th>
+                        <th></th>
+                        <th></th>
+                        </tr>
+                      </thead>
+                      <ContactList  contacts={this.props.contacts} />
+                </table>
+                );
+          }
+      });
+
+       var ContactsApp = React.createClass({
+          render: function(){
+              return (
+                    <div>
+                       <h1>Contact List.</h1>
+                       <ContactsTable  contacts={this.props.contacts} />
+                    </div>
+              );
+          }
+      });
+
+    var contacts = [
+            {
+                "name": "Contact 1",
+                "address": "123 Test St",
+                "phone_number": "132-3212"
+            },
+
+            {
+                "name": "Contact 2",
+                "address": "23 Main St",
+                "phone_number": "934-4329"
+            },
+
+            {
+                "name": "Contact 3",
+                "address": "4 Lower St",
+                "phone_number": "432-5832"
+            },
+
+            {
+                "name": "Contact 4",
+                "address": "49 Upper Street",
+                "phone_number": "934-4290"
+            }
+          ] ;       
+    // Changed code 
+    ReactDOM.render(
+      <ContactsApp  contacts={contacts}  />,
+      document.getElementById('mount-point')
+    );
--- apps/phoneCatalogue/app.js
+++ apps/phoneCatalogue/app.js
@@ -1,1 +1,72 @@
-// TODO
+var ReactDOM = require('react-dom')
+    var React = require('react')
+    var phones = require('./data').allPhones ;
+
+    var SelectBox = React.createClass({
+      render: function(){
+           return (
+                <div className="col-md-10">
+              <input type="text" placeholder="Search" />
+              Sort by:
+              <select>
+                <option value="name">Alphabetical</option>
+                <option value="age">Newest</option>
+              </select>
+             </div>
+               );
+          }
+       });
+
+
+    // TODO (missing component)
+    var PhoneItem= React.createClass({
+          render: function(){
+              return (
+                 <li className="thumbnail phone-listing">
+                <a href={'#/phones/'+this.props.phones.age} className="thumb">
+                  <img src={this.props.phones.imageUrl}/> </a>
+                <a href={'#/phones/'+this.props.phones.age}>{this.props.phones.id}</a>
+                     <p>{this.props.phones.snippet}</p>
+                 </li> 
+                ) ;
+          }
+      });
+
+
+     var FilteredPhoneList = React.createClass({
+          render: function(){
+              var displayedPhones = this.props.phones.map(function(phone) {
+                  return <PhoneItem key={phone.id} phones={phone} /> ;
+              }) ;
+              return (
+                      <div className="col-md-10">
+                        <ul className="phones">
+                            {displayedPhones}
+                        </ul>
+                      </div>
+                  ) ;
+          }
+      });
+
+    var PhoneCatalogueApp = React.createClass({
+      render: function(){
+        
+          return (
+                <div className="view-container">
+                <div className="view-frame">
+                   <div className="container-fluid">
+                   <div className="row">
+                       <SelectBox />
+                       <FilteredPhoneList phones={phones} />
+                  </div> 
+                  </div>                   
+                </div>
+              </div>
+          );
+      }
+    });
+
+    ReactDOM.render(
+      <PhoneCatalogueApp phones={phones} />,
+      document.getElementById('mount-point')
+    );
