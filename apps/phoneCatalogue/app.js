20c45509e065df85845e4f1c61d478b3486d15fc
Han Ni <nihan1994@163.com>
2016-03-03 19:44:17 +0000

    Phone app - search page complete

--- apps/phoneCatalogue/app.js
+++ apps/phoneCatalogue/app.js
@@ -1,17 +1,31 @@
 var ReactDOM = require('react-dom')
     var React = require('react')
     var phones = require('./data').allPhones ;
+    var _ = require('lodash');
 
     var SelectBox = React.createClass({
+      handleChange : function(e, type,value) {
+           e.preventDefault();
+           this.props.onUserInput( type,value);
+      },
+      handleTextChange : function(e) {
+            this.handleChange( e, 'search', e.target.value);
+      },
+      handleSortChange : function(e) {
+          this.handleChange(e, 'sort', e.target.value);
+      },
       render: function(){
            return (
                 <div className="col-md-10">
-              <input type="text" placeholder="Search" />
-              Sort by:
-              <select>
-                <option value="name">Alphabetical</option>
-                <option value="age">Newest</option>
-              </select>
+              <input type="text" placeholder="Search" 
+                          value={this.props.filterText}
+                          onChange={this.handleTextChange} />
+                 Sort by:
+                  <select id="sort" value={this.props.order } 
+                         onChange={this.handleSortChange} >
+                     <option value="name">Alphabetical</option>
+                     <option value="age">Newest</option>
+                 </select>
              </div>
                );
           }
@@ -49,15 +63,31 @@ var ReactDOM = require('react-dom')
       });
 
     var PhoneCatalogueApp = React.createClass({
+      getInitialState: function() {
+           return { search: '', sort: 'name' } ;
+      }, 
+      handleChange : function(type,value) {
+            if ( type == 'search' ) {
+                this.setState( { search: value } ) ;
+              } else {
+                 this.setState( { sort: value } ) ;
+              }
+      }, 
       render: function(){
-        
+        //console.log('Criteria: Search= ' + this.state.search + ' ; Sort= ' this.state.sort);
+        var list = this.props.phones.filter(function(p) {
+                  return p.name.toLowerCase().search(this.state.search.toLowerCase() ) != -1 ;
+                }.bind(this) );
+        var filteredList = _.sortBy(list, this.state.sort) ;
           return (
                 <div className="view-container">
                 <div className="view-frame">
                    <div className="container-fluid">
                    <div className="row">
-                       <SelectBox />
-                       <FilteredPhoneList phones={phones} />
+                       <SelectBox onUserInput={this.handleChange } 
+                             filterText={this.state.search} 
+                             sort={this.state.sort} />
+                       <FilteredPhoneList phones={filteredList} />
                   </div> 
                   </div>                   
                 </div>
