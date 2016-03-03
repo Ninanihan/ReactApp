e5d8202903ea6ca7ac8cf15122f35735350d39c0
Han Ni <nihan1994@163.com>
2016-03-03 20:12:44 +0000

    Hacker App - Add feature completed

--- apps/hackerNews/app.js
+++ apps/hackerNews/app.js
@@ -1,7 +1,29 @@
 var ReactDOM = require('react-dom')
-    var React = require('react')
+var React = require('react')
+var api =  require ('./stubAPI').api;
+var _ = require('lodash');
 
     var Form = React.createClass({
+      getInitialState: function() {
+                   return { title: '', link: ''};
+                },
+      addTitle: function(e) {
+                    this.setState({title: e.target.value});
+                },
+      addLink: function(e) {
+                    this.setState({link: e.target.value});
+                },
+      addRecord: function(e){
+                    e.preventDefault();
+                    var title = this.state.title.trim();
+                    var link = this.state.link.trim();
+                    if (!title) {
+                            return;
+                    }
+                    this.props.addPostHandler(title,link);
+                    this.setState({title: "",link: ""});
+                },
+
           render : function() {
                return (
                    <form style={{marginTop: '30px'}}>
@@ -9,38 +31,42 @@ var ReactDOM = require('react-dom')
 
                       <div className="form-group">
                         <input type="text"
-                          className="form-control"
-                          placeholder="Title"></input>
+                          className="form-control"  placeholder="Title"
+                              value={this.state.title} onChange={this.addTitle}></input>
                       </div>
                       <div className="form-group">
                         <input type="text"
                         className="form-control"
-                        placeholder="Link"></input>
+                        placeholder="Link"
+                        value={this.state.link} onChange={this.addLink}></input>
                       </div>
-                      <button type="submit" className="btn btn-primary">Post</button>
+                      <button type="submit" className="btn btn-primary" onClick={this.addRecord} >Post</button>
                     </form>
                    );
             }
      });
 
      var NewsItem = React.createClass({
+      handleVote : function() {
+                     this.props.upvoteHandler(this.props.post.id);
+                },
+
           render : function() {
-              var divStyle = {
-                   fontSize: '20px', 
-                   marginLeft: '10px' 
-                  };
+              var lineStyle = {
+                         fontSize: '20px', marginLeft: '10px'  };
               var line ;
               if (this.props.post.link ) {
                  line = <a href={this.props.post.link} >
-                {            this.props.post.title} </a> ;
+                             {this.props.post.title} </a> ;
               } else {
                  line = <span>{this.props.post.title} </span> ;
               }
               return (
                   <div >
-                    <span className="glyphicon glyphicon-thumbs-up"></span>
+                    <span className="glyphicon glyphicon-thumbs-up"
+                    onClick={this.handleVote} ></span>
                     {this.props.post.upvotes}
-                    <span style={divStyle} >{line}<span>
+                    <span style={lineStyle} >{line}<span>
                         <a href={'#/posts/' + this.props.post.id }>Comments</a>
                       </span>
                     </span>
@@ -53,10 +79,9 @@ var ReactDOM = require('react-dom')
     var NewsList = React.createClass({
                 render : function() {
                   var items = this.props.posts.map(function(post,index) {
-                         return (
-                         	<NewsItem key={index} post={post} 
-                                  posts={post}  /> 
-                                  );
+                         return <NewsItem key={index} post={post} 
+                                  upvoteHandler={this.props.upvoteHandler} /> ;
+                                  
                      }.bind(this) )
                   return (
                     <div>
@@ -67,15 +92,28 @@ var ReactDOM = require('react-dom')
            }) ;
 
      var HackerApp = React.createClass({
+      incrementUpvote : function(id) {
+                   api.upvote(id) ;
+                   this.setState({});
+              },
+      addPost : function(title,link){
+                   api.add(title,link) ;
+                   this.setState({});
+              }, 
         render: function(){
+          var posts = _.sortBy(api.getAll(), function(post) {
+                          return - post.upvotes;
+                       }
+                    );
             return (
                     <div className="container">
                       <div className="row">
                         <div className="col-md-6 col-md-offset-3">
                            <div className="page-header">
                              <h1>Hacker News</h1>
-                                   <NewsList posts={posts}  />
-                                   <Form  addPostHandler={this.addPost}/>
+                                   <NewsList posts={posts} 
+                                   upvoteHandler={this.incrementUpvote} />
+                                   <Form  addPostHandler={this.addPost} />
                            </div>
                        </div>
                       </div>
@@ -84,37 +122,9 @@ var ReactDOM = require('react-dom')
         }
     });
 
-    var posts = [
-             {   title : 'India - Tiger population sees 30% increase.',
-                link : 'http://www.bbc.com/news/world-asia-30896028',
-                username : 'jbloggs',
-                comments : [],
-                upvotes : 10
-              },
-             { 
-                title : 'The button that is not.',
-                link : 'http://blog.nuclearsecrecy.com/2014/12/15/button-isnt/',
-                username : 'notme',
-                comments : [],
-                upvotes : 12
-              },
-              { 
-                title : 'Google Nears $1B Investment in SpaceX',
-                link : null,
-                username : 'notme',
-                comments : [],
-                upvotes : 12
-              },
-              { 
-                title : 'Coinbase Raises $75M from DFJ Growth, USAA, and More',
-                link : 'http://blog.coinbase.com/post/108642362357/coinbase-raises-75m-from-dfj-growth-usaa-nyse',
-                username : 'psmith',  
-                comments : [],
-                upvotes : 2
-              }
-          ] ;
+    
 
     ReactDOM.render(
-      <HackerApp posts={posts} />,
+      <HackerApp />,
       document.getElementById('mount-point')
     );
